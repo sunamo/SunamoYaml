1ffb1aefa3b0aac7a7031f9c308eff62fff38f67
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:40:14 +0100

    Remove remain unused method

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -3,95 +3,11 @@ namespace SunamoYaml._sunamo.SunamoExceptions;
 internal sealed partial class Exceptions
 {
     #region Other
-    internal static string CheckBefore(string before)
-    {
-        return string.IsNullOrWhiteSpace(before) ? string.Empty : before + ": ";
-    }
 
     internal static Tuple<string, string, string> PlaceOfException(
 bool fillAlsoFirstTwo = true)
-    {
-        StackTrace st = new();
-        var v = st.ToString();
-        var l = v.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        l.RemoveAt(0);
-        var i = 0;
-        string type = string.Empty;
-        string methodName = string.Empty;
-        for (; i < l.Count; i++)
-        {
-            var item = l[i];
-            if (fillAlsoFirstTwo)
-                if (!item.StartsWith("   at ThrowEx"))
-                {
-                    TypeAndMethodName(item, out type, out methodName);
-                    fillAlsoFirstTwo = false;
-                }
-            if (item.StartsWith("at System."))
-            {
-                l.Add(string.Empty);
-                l.Add(string.Empty);
-                break;
-            }
-        }
-        return new Tuple<string, string, string>(type, methodName, string.Join(Environment.NewLine, l));
-    }
-    internal static void TypeAndMethodName(string l, out string type, out string methodName)
-    {
-        var s2 = l.Split("at ")[1].Trim();
-        var s = s2.Split("(")[0];
-        var p = s.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        methodName = p[^1];
-        p.RemoveAt(p.Count - 1);
-        type = string.Join(".", p);
-    }
-    internal static string CallingMethod(int v = 1)
-    {
-        StackTrace stackTrace = new();
-        var methodBase = stackTrace.GetFrame(v)?.GetMethod();
-        if (methodBase == null)
-        {
-            return "Method name cannot be get";
-        }
-        var methodName = methodBase.Name;
-        return methodName;
-    }
-    #endregion
-
-    #region IsNullOrWhitespace
-    internal static string? IsNullOrWhitespace(string before, string argName, string argValue, bool notAllowOnlyWhitespace)
-    {
-        string addParams;
-        if (argValue == null)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is null" + addParams;
-        }
-        if (argValue == string.Empty)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is empty (without trim)" + addParams;
-        }
-        if (notAllowOnlyWhitespace && argValue.Trim() == string.Empty)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is empty (with trim)" + addParams;
-        }
-        return null;
-    }
+{}    #region IsNullOrWhitespace
     readonly static StringBuilder sbAdditionalInfoInner = new();
     readonly static StringBuilder sbAdditionalInfo = new();
-    internal static string AddParams()
-    {
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfo.Insert(0, "Outer:");
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, "Inner:");
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        var addParams = sbAdditionalInfo.ToString();
-        var addParamsInner = sbAdditionalInfoInner.ToString();
-        return addParams + addParamsInner;
-    }
     #endregion
 }
--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -4,59 +4,7 @@ internal partial class ThrowEx
 
 
     #region Other
-    internal static string FullNameOfExecutedCode()
-    {
-        Tuple<string, string, string> placeOfExc = Exceptions.PlaceOfException();
-        string f = FullNameOfExecutedCode(placeOfExc.Item1, placeOfExc.Item2, true);
-        return f;
-    }
 
-    static string FullNameOfExecutedCode(object type, string methodName, bool fromThrowEx = false)
-    {
-        if (methodName == null)
-        {
-            int depth = 2;
-            if (fromThrowEx)
-            {
-                depth++;
-            }
 
-            methodName = Exceptions.CallingMethod(depth);
-        }
-        string typeFullName;
-        if (type is Type type2)
-        {
-            typeFullName = type2.FullName ?? "Type cannot be get via type is Type type2";
-        }
-        else if (type is MethodBase method)
-        {
-            typeFullName = method.ReflectedType?.FullName ?? "Type cannot be get via type is MethodBase method";
-            methodName = method.Name;
-        }
-        else if (type is string)
-        {
-            typeFullName = type.ToString() ?? "Type cannot be get via type is string";
-        }
-        else
-        {
-            Type t = type.GetType();
-            typeFullName = t.FullName ?? "Type cannot be get via type.GetType()";
-        }
-        return string.Concat(typeFullName, ".", methodName);
-    }
-
-    internal static bool ThrowIsNotNull(string? exception, bool reallyThrow = true)
-    {
-        if (exception != null)
-        {
-            Debugger.Break();
-            if (reallyThrow)
-            {
-                throw new Exception(exception);
-            }
-            return true;
-        }
-        return false;
-    }
     #endregion
 }
