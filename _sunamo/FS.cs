0db7084266ad902855eba955457c690f8222909d
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 21:01:08 +0100

    Buildable, removed unused internal,private,protected methods in _sunamo

--- _sunamo/FS.cs
+++ _sunamo/FS.cs
@@ -3,37 +3,4 @@ namespace SunamoYaml._sunamo;
 //namespace SunamoYaml._sunamo.SunamoExceptions._AddedToAllCsproj;
 internal class FS
 {
-    internal static void CreateFoldersPsysicallyUnlessThere(string nad)
-    {
-        ThrowEx.IsNullOrEmpty("nad", nad);
-        //ThrowEx.IsNotWindowsPathFormat("nad", nad);
-        if (Directory.Exists(nad))
-        {
-            return;
-        }
-        List<string> slozkyKVytvoreni = new List<string>
-{
-nad
-};
-        while (true)
-        {
-            nad = Path.GetDirectoryName(nad);
-            
-            if (Directory.Exists(nad))
-            {
-                break;
-            }
-            string kopia = nad;
-            slozkyKVytvoreni.Add(kopia);
-        }
-        slozkyKVytvoreni.Reverse();
-        foreach (string item in slozkyKVytvoreni)
-        {
-            string folder = item;
-            if (!Directory.Exists(folder))
-            {
-                Directory.CreateDirectory(folder);
-            }
-        }
-    }
 }
